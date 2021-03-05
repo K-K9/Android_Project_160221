ed145593a6f375b85382d58dfa563679605a29c8
MiiroSilander <miiro.silander@gmail.com>
2021-03-05 14:13:52 +0200

    Muokkausta päivämäärä ja aika syötteeseen

--- app/src/main/java/com/example/project1602/MittausTulokset.java
+++ app/src/main/java/com/example/project1602/MittausTulokset.java
@@ -1,17 +1,23 @@
 package com.example.project1602;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
+
+
+
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+
 import java.util.Date;
 import java.util.Locale;
 
 public class MittausTulokset extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,31 +26,17 @@ public class MittausTulokset extends AppCompatActivity {
 
 
 
-
-
-
-        //Päivämäärä ja aika tekstikentät
         TextView AikaTextView = findViewById(R.id.textViewAika);
-        TextView PaivamaaraTextView = findViewById(R.id.textViewPaivamaara);
-        //asettaa päivämäärän ja ajan tekstikenttiin
+        //hakee päivämäärän ja ajan kalenterista,   "pattern" miten halutaan ajan tulevan näkyvin
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy     HH:mm", Locale.getDefault());
+        String currentTime = sdf.format(new Date());
         AikaTextView.setText(currentTime);
-        PaivamaaraTextView.setText(currentDate);
-
 
 
     }
 
-    //hakee päivämäärän ja ajan kalenterista,   "pattern" miten halutaan ajan tulevan näkyvin
-    String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-    String currentTime = new SimpleDateFormat("HH:mm", Locale.getDefault()).format(new Date());
 
 
 
-
-    /*//buttonille testi siirtyminen mittaustulosten aktiviteettiin
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, MittausTulokset.class);
-        startActivity(intent);
-    }*/
 
 }
